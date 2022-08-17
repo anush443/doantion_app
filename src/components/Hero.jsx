e4b37082660a19be2ea9fc9a972185816da2dfda
anushgs <anush443@gmail.com>
2022-08-18 00:52:09 +0530

    Yes

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -9,30 +9,6 @@ const Hero = () => {
     const toggle = () => setModal(!modal)
     return (
         <>
-            {/* <div className="flex flex-col items-center justify-center h-screen md:flex-row bg-gradient-to-r from-indigo-500">
-                <div className="flex mt-5 h-1/3 md:h-2/3 justify-center  md:mt-0">
-                    <img src={ethlogo} alt="heroimg" className="object-fit" />
-                </div>
-                <div className="flex  flex-col  p-2 space-y-5 text-center md:text-left  ">
-                    <h3 className="text-4xl font-serif font-bold   tracking-widest">
-                        Fund Your Favorite Creators
-                    </h3>
-                    <h3 className="text-4xl  font-serif font-bold md:text-left  tracking-widest">
-                        Decentarziled funding
-                    </h3>
-                    <div className="flex flex-col md:flex-row mt-10  space-y-3 md:space-x-6 md:space-y-0 ">
-                        <button className="px-6 py-2 rounded-lg text-lg font-medium text-white bg-gradient-to-r from-pink-400 to-yellow-500 hover:from-green-500  hover:to-blue-500 duration-500">
-                            Discover Creators
-                        </button>
-                        <button
-                            onClick={toggle}
-                            className="px-6 py-2 rounded-lg  text-lg font-medium text-white bg-gradient-to-r from-pink-400 to-yellow-500 hover:from-green-500  hover:to-blue-500 duration-500"
-                        >
-                            Create Account
-                        </button>
-                    </div>
-                </div>
-            </div> */}
             <section id="hero" className="bg-gradient-to-r from-indigo-500">
                 <div className="max-w-6xl mx-auto text-center min-h-screen mb-40 pt-12 ">
                     <img src={ethlogo} alt="heroimg" className="mx-auto h-48 my-12" />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -23,25 +23,6 @@ const CustomNavbar = ({ args }) => {
     const toggle = () => setIsOpen(!isOpen)
     return (
         <>
-            {/* <div className="flex flex-row p-3 h-20 text-white items-center  bg-zinc-800 ">
-                <div className="flex flex-row ml-3">
-                    <h3 className=" text-xl ">FunDmE</h3>
-                </div>
-                <div className="flex flex-row items-center  mx-auto space-x-4">
-                    <Link to="/">
-                        <h3 className=" text-xl  hover:text-violet-700 duration-500">Home</h3>
-                    </Link>
-                    <Link to="/dashboard">
-                        <h3 className=" text-xl  hover:text-violet-700 duration-500">Dashboard</h3>
-                    </Link>
-                    <Link to="/discover">
-                        <h3 className=" text-xl  hover:text-violet-700 duration-500">Discover</h3>
-                    </Link>
-                </div>
-                <div className=" items-center">
-                    <ConnectButton />
-                </div>
-            </div> */}
             <div>
                 <Navbar {...args}>
                     <NavbarBrand href="/">GoFund</NavbarBrand>
